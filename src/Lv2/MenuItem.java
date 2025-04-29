2131544fe92023601ab54df13882cd0e316aaf6b
yebitt <yebin0519@naver.com>
2025-04-29 16:02:39 +0900

    1. MenuItem 클래스 작성 \n 2. main함수 작성 - List선언 및 객체(메뉴) 추가 , 반복문을 통한 객체(메뉴) 출력

--- src/Lv2/MenuItem.java
+++ src/Lv2/MenuItem.java
@@ -1,4 +1,24 @@
 package Lv2;
 
 public class MenuItem {
+    private String name;
+    private double price;
+    private String explain;
+
+    public MenuItem(String name, double price, String explain){
+        this.name = name;
+        this.price = price;
+        this.explain = explain;
+    }
+
+    // getter, setter
+    public String getName(){
+        return name;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public String getExplain(){
+        return explain;
+    }
 }
